7326a2fef7c0b5fcf446e97fb63c7f9efbe16ba3
Pavel Ryabykh <pryabykh@gmail.com>
2025-06-08 14:54:47 +0300

    Создание каркаса фронта

---
+++ front/src/main/java/com/pryabykh/bankapp/front/configuration/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.pryabykh.bankapp.front.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/")
+                        .permitAll()
+                )
+                .logout(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
---
+++ front/src/main/java/com/pryabykh/bankapp/front/controller/BankAppController.java
@@ -0,0 +1,17 @@
+package com.pryabykh.bankapp.front.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class BankAppController {
+
+    @GetMapping()
+    public String main() {
+        return "main";
+    }
+}
