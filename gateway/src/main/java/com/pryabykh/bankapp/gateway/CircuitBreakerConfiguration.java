2c30b6c565e0e23970bcc7360f5a1a10af123d71
Pavel Ryabykh <pryabykh@gmail.com>
2025-06-24 20:33:01 +0300

    Circuit breaker

---
+++ gateway/src/main/java/com/pryabykh/bankapp/gateway/CircuitBreakerConfiguration.java
@@ -0,0 +1,25 @@
+package com.pryabykh.bankapp.gateway;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class CircuitBreakerConfiguration {
+    @Value("${circuit-breaker.timeout-seconds}")
+    Long circuitBreakerTimeout;
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(circuitBreakerTimeout)).build()).build());
+    }
+}
